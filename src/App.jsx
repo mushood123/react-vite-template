5c9a40e44db78b9a4764926f80c32e8bc32c3d80
mushood123 <khawaja.muhammad.mushood>
2025-10-03 11:07:01 +0500

    Fix: Remove unused imports and test pre-commit hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { del, get, patch, post, put } from './api'
+// import { del, get, patch, post, put } from './api'
 
 function App() {
   return (
-
+    <div>
+      <h1>Hello World</h1>
+    </div>
   )
 }
 
